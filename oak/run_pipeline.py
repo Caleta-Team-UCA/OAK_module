913c41e0424870b27a216dbb2f4d5dce4769f989
Javier-Jimenez99 <javijj99@gmail.com>
2021-07-14 16:43:04 +0200

    change frequency to seconds

--- oak/run_pipeline.py
+++ oak/run_pipeline.py
@@ -1,8 +1,8 @@
-from time import sleep
+from time import sleep, time
 
 import typer
 
-from oak.utils.series import PlotSeries
+from oak.utils.results import PlotSeries
 from oak.pipeline.oak_cam import OAKCam
 from oak.pipeline.oak_video import OAKVideo
 from oak.process.activity import Activity
@@ -14,8 +14,8 @@ def main(
     face_path_model: str = "models/face-detection-openvino_2021.2_4shave.blob",
     stress_path_model: str = "models/mobilenet_stress_classifier_2021.2.blob",
     video_path: str = "videos/22-center-2.mp4",
-    frequency: int = 12,
-    size: int = 240,
+    frequency: int = 5,
+    plot_results: bool = True,
 ):
     """Runs the OAK pipeline, streaming from a video file or from the camera, if
     no video file is provided. The pipeline shows on screen the video on real time,
@@ -34,29 +34,28 @@ def main(
         "models/stress_classifier_2021.2.blob"
     video_path : str, optional
         Path to the video file. If None provided, uses the cam. By default None
-    size : int, optional
-        Length of the stored series, by default 1000
     frequency : int, optional
-        Rate at which plots are updated, in frames, by default 24
+        Rate at which plots are updated, in seconds, by default 5
+    plot_results : bool, optional
+        Whether to plot results or not.
     """
-    act = Activity(size=size, frequency=frequency)
-    stre = Stress(size=size, frequency=frequency)
+    act = Activity()
+    stre = Stress()
 
-    plot_series = PlotSeries(
-        [stre.ser_score, act.ser_score],
-        xlim=(0, size),
-        ylim=(-0.15, 1.15),
-    )
+    if plot_results:
+        plot_series = PlotSeries([stre, act])
 
     if video_path is None:
         processor = OAKCam(body_path_model, face_path_model, stress_path_model)
-        processor_parameters = {"show_results": True}
+        processor_parameters = {"show_results": plot_results}
     else:
         processor = OAKVideo(body_path_model, face_path_model, stress_path_model)
-        processor_parameters = {"video_path": video_path, "show_results": True}
+        processor_parameters = {"video_path": video_path, "show_results": plot_results}
+
+    start_time = time()
 
     for i, result in enumerate(processor.get(**processor_parameters)):
-        # Process activity
+        # Process activityq
         if result.body_detection is not None and result.face_detection is not None:
             act.update(result.body_detection, result.face_detection)
 
@@ -69,12 +68,15 @@ def main(
             # TODO: Aquí habrá que añadir el código de la parte de BREATH
             pass
 
-        if i % frequency == 0:
+        if plot_results and time() - start_time >= frequency:
             plot_series.update("movavg")
+            act.restart_series()
+            stre.restart_series()
+            start_time = time()
 
         # Aquí simulamos que se estuviera haciendo
         # algún tipo de procesamiento con los datos
-        sleep(0.1)
+        sleep(0.05)
 
 
 if __name__ == "__main__":
