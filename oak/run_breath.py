b8806faf2b95583fc22ea851f3768cbe5e95e885
Daniel Precioso <danipg1409@gmail.com>
2021-07-15 10:49:45 +0200

    Run breath script

---
+++ oak/run_breath.py
@@ -0,0 +1,26 @@
+import typer
+
+from oak.pipeline.oak_cam import OAKCam
+from oak.process.breath import Breath, BreathConfig
+
+
+def main(
+    body_path_model: str = "models/mobilenet-ssd_openvino_2021.2_8shave.blob",
+    face_path_model: str = "models/face-detection-openvino_2021.2_4shave.blob",
+    stress_path_model: str = "models/mobilenet_stress_classifier_2021.2.blob",
+    video_path: str = None,
+):
+    breath_config = BreathConfig()
+    breath = Breath(breath_config)
+    if video_path is None:
+        processor = OAKCam(body_path_model, face_path_model, stress_path_model)
+        for i, result in enumerate(processor.get(True)):
+            breath.update(
+                result.face_detection, result.depth, result.calculator_results
+            )
+    else:
+        raise NameError("InvalidOption")
+
+
+if __name__ == "__main__":
+    typer.run(main)
