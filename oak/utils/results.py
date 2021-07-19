c97bd388db02638814b447e225ae6cf9f3dceb59
Daniel Precioso <danipg1409@gmail.com>
2021-07-19 20:27:17 +0200

    Results now plot many series

--- oak/utils/results.py
+++ oak/utils/results.py
@@ -27,12 +27,7 @@ class PlotSeries:
             self.ser_plot_data[ser.name]["color"] = c
             self.ser_plot_data[ser.name]["ax"] = ax
 
-            ax.plot(
-                ser.score.index.to_numpy(),
-                ser.get_moving_average(),
-                self.ser_plot_data[ser.name]["color"],
-                label=ser.name,
-            )
+            self._plot_ser(ser, ax)
 
             ax.grid()
             ax.set_xlabel("Time (frames)")
@@ -41,17 +36,29 @@ class PlotSeries:
         plt.tight_layout()
         plt.show(block=False)
 
+    def _plot_ser(self, ser, ax):
+        moving_mean = ser.get_moving_average()
+        if type(moving_mean) == dict:
+            for i, (name, ser_sub) in enumerate(moving_mean.items()):
+                ax.plot(
+                    ser.score.index.to_numpy(),
+                    ser_sub,
+                    COLORS[i],
+                    label=name,
+                )
+            ax.legend(moving_mean.keys())
+        else:
+            ax.plot(
+                ser.score.index.to_numpy(),
+                moving_mean,
+                self.ser_plot_data[ser.name]["color"],
+                label=ser.name,
+            )
+
     def update(self, method: str = None):
         """Updates the figure"""
         for name, ser_data in self.ser_plot_data.items():
-            ser = ser_data["object"]
-            moving_mean = ser.get_moving_average()
-            ser_data["ax"].plot(
-                ser.score.index.to_numpy(),
-                moving_mean,
-                ser_data["color"],
-                label=name,
-            )
+            self._plot_ser(ser_data["object"], ser_data["ax"])
 
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
