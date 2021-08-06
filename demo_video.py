1a34d40b66eb3021a4facac0a159baf921a0b72b
Daniel Precioso <danipg1409@gmail.com>
2021-08-06 11:40:37 +0200

    Demo scripts

---
+++ demo_video.py
@@ -0,0 +1,16 @@
+from oak.run_pipeline import run_pipeline
+
+# This is a demostration script
+#
+# To run it, you need to have the OAK device plugged into your device
+# You also need to install our "oak" environment first
+# You can find an in-depth explanation in the README
+#
+# Once running, the script:
+# 1. Loads the videos from the "demo" folder
+# 2. Performs body and face detection
+# 3. Computes the scores related to stress, activity and breath
+# 4. Shows the scores on real-time
+
+if __name__ == "__main__":
+    run_pipeline(video_path="demo/sample", post_server=False)
